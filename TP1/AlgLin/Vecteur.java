1670d81103a7694a24fd8069d1a834b410f18e60
Quentin V <quentin.vauthier000@gmail.com>
2021-01-20 15:04:31 +0100

    Ajout de 2 constructeurs et méthodes nbLigne, getCoef, remplacerCoef, toString, scalaire

--- TP1/AlgLin/Vecteur.java
+++ TP1/AlgLin/Vecteur.java
@@ -1,15 +1,50 @@
 package AlgLin;
 
 public class Vecteur extends Matrice {
-    Vecteur(int nbligne, int nbcolonne) {
-        super(nbligne, nbcolonne);
-    }
 
-    Vecteur(double[][] tableau) {
-        super(tableau);
-    }
+	Vecteur(int nbligne) {
+		super(nbligne, 1);
+	}
 
-    Vecteur(String fichier) {
-        super(fichier);
-    }
+	Vecteur(double[] tableau) {
+		super(tableau.length, 1);
+		for (int i = 0; i < tableau.length; ++i) {
+			this.coefficient[i][0] = tableau[i];
+		}
+	}
+
+	@Override
+	public int nbLigne() {
+		return super.nbLigne();
+	}
+
+	public double getCoef(int ligne) {
+		return super.getCoef(ligne, 0);
+	}
+
+	@Override
+	public void remplacecoef(int ligne, int colonne, double value) {
+		super.remplacecoef(ligne, 0, value);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+	static double scalaire(Vecteur v1, Vecteur v2) {
+		double scal = 0;
+		int v1T = v1.nbLigne();
+		int v2T = v2.nbLigne();
+		int grandeTaille = Math.max(v1T, v2T);
+		for(int i = 0; i < grandeTaille; ++i) {
+			if(i > v1T || i > v2T) continue;
+			scal += v1.coefficient[i][0] * v2.coefficient[i][0];
+		}
+		return scal;
+	}
+
+	public static void main(String[] args) {
+
+	}
 }
